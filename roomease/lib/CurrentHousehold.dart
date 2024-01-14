abe60212b84dd54cd41cdb1ab06fb4a6ffa6a9ec
Kenneth Yang <k233yang@uwaterloo.ca>
2024-01-14 16:50:21 -0500

    missing files

---
+++ roomease/lib/CurrentHousehold.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:roomease/SharedPreferencesUtility.dart';
+import 'Household.dart';
+
+class CurrentHousehold {
+  static Household getCurrentHousehold() {
+    String householdId = SharedPreferencesUtility.getString("householdId");
+    String householdName = SharedPreferencesUtility.getString("householdName");
+    return Household(householdName, householdId, List.empty());
+  }
+
+  static String getCurrentHouseholdId() {
+    return SharedPreferencesUtility.getString("householdId");
+  }
+
+  static String getCurrentHouseholdName() {
+    return SharedPreferencesUtility.getString("householdName");
+  }
+
+  static void setCurrentHouseholdId(String householdId) {
+    SharedPreferencesUtility.setValue("householdId", householdId);
+  }
+
+  static void setCurrentHouseholdName(String householdName) {
+    SharedPreferencesUtility.setValue("householdName", householdName);
+  }
+}
---
+++ roomease/lib/profile/ProfileScreen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:roomease/CurrentHousehold.dart';
+import 'package:roomease/CurrentUser.dart';
+import 'package:roomease/SharedPreferencesUtility.dart';
+import 'package:roomease/colors/ColorConstants.dart';
+import 'package:roomease/welcome/JoinHousehold.dart';
+
+class Profile extends StatefulWidget {
+  @override
+  State createState() {
+    return _ProfileState();
+  }
+}
+
+class _ProfileState extends State<Profile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Profile'),
+          backgroundColor: ColorConstants.lightPurple,
+        ),
+        body: Padding(
+            padding: EdgeInsets.only(top: 20, bottom: 20),
+            child: Column(
+                children: [profileSection(), logOutButton(context)],
+                crossAxisAlignment: CrossAxisAlignment.start)));
+  }
+}
+
+Widget profileSection() {
+  return Row(
+    children: [
+      Padding(
+          padding: EdgeInsets.only(left: 50),
+          child: Image(image: AssetImage('assets/user_profile_icon.png'))),
+      Padding(
+          padding: EdgeInsets.only(left: 30, right: 50),
+          child: profileDetails())
+    ],
+  );
+}
+
+Widget profileDetails() {
+  return Column(
+    children: [
+      Text(CurrentUser.getCurrentUserName()),
+      Text("Member of: ${CurrentHousehold.getCurrentHouseholdName()}"),
+      Text("Edit Information")
+    ],
+  );
+}
+
+Widget logOutButton(BuildContext context) {
+  return TextButton(
+      onPressed: () {
+        SharedPreferencesUtility.clear();
+        Navigator.pushNamedAndRemoveUntil(context, "/welcome", (_) => false);
+      },
+      child: Text("Log Out"));
+}
