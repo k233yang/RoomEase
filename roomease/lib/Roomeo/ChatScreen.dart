f28b7a11921c1472141b43b52ded7b7c5c4b4e22
cakec <cakecheese12@gmail.com>
2024-02-05 17:56:03 -0500

    prettier chat screen

--- roomease/lib/Roomeo/ChatScreen.dart
+++ roomease/lib/Roomeo/ChatScreen.dart
@@ -7,6 +7,8 @@ import '../Message.dart';
 import '../colors/ColorConstants.dart';
 import 'package:chat_bubbles/chat_bubbles.dart';
 import 'package:roomease/Roomeo/Roomeo.dart';
+import 'package:grouped_list/grouped_list.dart';
+import 'package:intl/intl.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -28,17 +30,6 @@ class _ChatScreen extends State<ChatScreen> {
     scrollController = ScrollController();
     textFieldFocusNode = FocusNode();
     // Scroll to the bottom when the widget is built
-    SchedulerBinding.instance.addPostFrameCallback((_) {
-      Future.delayed(const Duration(milliseconds: 200), () {
-        if (scrollController.hasClients) {
-            scrollController.animateTo(
-            scrollController.position.maxScrollExtent,
-            duration: Duration(milliseconds: 200),
-            curve: Curves.easeOut,
-          );
-        }
-      });
-    });
   }
 
   @override
@@ -54,13 +45,27 @@ class _ChatScreen extends State<ChatScreen> {
           title: const Text('Chat with Roomeo'),
           backgroundColor: ColorConstants.lightPurple,
         ),
-        body: Container(
-            color: ColorConstants.white,
-            child: Column(children: [
-              DatabaseManager.messagesStreamBuilder(
-                  CurrentUser.getCurrentUserId() + RoomeoUser.user.userId),
-              Center(child: chatTextField()),
-            ])));
+        body: Column(
+          children: [
+            Expanded(
+                child: DatabaseManager.messagesStreamBuilder(
+                    CurrentUser.getCurrentUserId() + RoomeoUser.user.userId)),
+            Container(
+              color: ColorConstants.white,
+              child: chatTextField(),
+            ),
+          ],
+        )
+
+        // Container(
+        //   color: ColorConstants.white,
+        //   child: Column(children: [
+        //     DatabaseManager.messagesStreamBuilder(
+        //         CurrentUser.getCurrentUserId() + RoomeoUser.user.userId),
+        //     Center(child: chatTextField()),
+        //   ]),
+        // ),
+        );
   }
 
   Widget chatTextField() {
@@ -68,41 +73,23 @@ class _ChatScreen extends State<ChatScreen> {
         focusNode: textFieldFocusNode,
         controller: _controller,
         onSubmitted: (String message) async {
-          // add the user message to the database
-          DateTime dateTime = DateTime.now();
-          messageList.add(Message(message, CurrentUser.getCurrentUserId(),
-              CurrentUser.getCurrentUserName(), dateTime));
-          _controller.clear();
-          textFieldFocusNode.requestFocus();
-          setState(() {});
-          // Scroll to the bottom when a new message is added
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (scrollController.hasClients) {
-              scrollController.animateTo(
-                scrollController.position.maxScrollExtent,
-                duration: Duration(milliseconds: 500),
-                curve: Curves.easeOut,
-              );
-            }
-          });
-          // add user + Roomeo response to the DB
-          await getRoomeoResponse(message, dateTime);
-          // fetch the category of the message
-          String category = await getCommandCategory(message);
-          print("CATEGORY IS: $category");
-          Map<String, dynamic> commandParams =
-              await getCommandParameters(category, message);
-          print(commandParams);
-
-          WidgetsBinding.instance.addPostFrameCallback((_) {
-            if (scrollController.hasClients) {
-              scrollController.animateTo(
-                scrollController.position.maxScrollExtent,
-                duration: Duration(milliseconds: 500),
-                curve: Curves.easeOut,
-              );
-            }
-          });
+          if (message != "") {
+            // add the user message to the database
+            DateTime dateTime = DateTime.now();
+            messageList.add(Message(message, CurrentUser.getCurrentUserId(),
+                CurrentUser.getCurrentUserName(), dateTime));
+            _controller.clear();
+            textFieldFocusNode.requestFocus();
+            setState(() {});
+            // add user + Roomeo response to the DB
+            await getRoomeoResponse(message, dateTime);
+            // fetch the category of the message
+            String category = await getCommandCategory(message);
+            print("CATEGORY IS: $category");
+            Map<String, dynamic> commandParams =
+                await getCommandParameters(category, message);
+            print(commandParams);
+          }
         },
         decoration: InputDecoration(
             filled: true,
@@ -117,30 +104,68 @@ class _ChatScreen extends State<ChatScreen> {
 }
 
 Widget buildListMessage(List<Message> messages) {
-  return Flexible(
-      child: ListView.builder(
-    itemCount: messages.length,
-    controller: _ChatScreen.scrollController,
-    itemBuilder: (context, index) => chatMessage(messages[index]),
-  ));
-}
-
-Widget chatMessage(Message message) {
-  MainAxisAlignment alignment;
-  if (message.senderId == CurrentUser.getCurrentUserId()) {
-    alignment = MainAxisAlignment.end;
-  } else {
-    alignment = MainAxisAlignment.start;
-  }
-  return Row(
-    mainAxisAlignment: alignment,
-    children: [
-      BubbleSpecialThree(
-        isSender: message.senderId == CurrentUser.getCurrentUserId(),
-        text: message.text,
-        color: ColorConstants.lightPurple,
-        textStyle: TextStyle(color: Colors.white, fontSize: 16),
-      )
-    ],
+  return GroupedListView<Message, DateTime>(
+    padding: const EdgeInsets.all(8),
+    reverse: true,
+    order: GroupedListOrder.DESC,
+    useStickyGroupSeparators: true,
+    floatingHeader: true,
+    elements: messages,
+    groupBy: (message) => DateTime(
+        message.timestamp.year, message.timestamp.month, message.timestamp.day),
+    groupHeaderBuilder: (Message message) => SizedBox(
+      height: 40,
+      child: Center(
+        child: Card(
+          color: ColorConstants.darkPurple,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              DateFormat.yMMMd().format(message.timestamp),
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ),
+    itemBuilder: (context, Message message) => Align(
+      alignment: message.senderId == CurrentUser.getCurrentUserId()
+          ? Alignment.centerRight
+          : Alignment.centerLeft,
+      child: ConstrainedBox(
+        constraints:
+            BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.75),
+        child: Card(
+          color: message.senderId == CurrentUser.getCurrentUserId()
+              ? ColorConstants.lightPurple
+              : ColorConstants.lighterGray,
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(message.text),
+          ),
+        ),
+      ),
+    ),
   );
 }
+
+// Widget chatMessage(Message message) {
+//   MainAxisAlignment alignment;
+//   if (message.senderId == CurrentUser.getCurrentUserId()) {
+//     alignment = MainAxisAlignment.end;
+//   } else {
+//     alignment = MainAxisAlignment.start;
+//   }
+//   return Row(
+//     mainAxisAlignment: alignment,
+//     children: [
+//       BubbleSpecialThree(
+//         isSender: message.senderId == CurrentUser.getCurrentUserId(),
+//         text: message.text,
+//         color: ColorConstants.lightPurple,
+//         textStyle: TextStyle(color: Colors.white, fontSize: 16),
+//       )
+//     ],
+//   );
+// }
