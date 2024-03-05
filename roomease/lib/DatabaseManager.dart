91082a2e769e352012b53abc3ad1cd7594096f30
Sookeong Cho <skcho@uwaterloo.ca>
2024-03-05 13:09:31 -0500

    Enable users to delete household

--- roomease/lib/DatabaseManager.dart
+++ roomease/lib/DatabaseManager.dart
@@ -548,6 +548,12 @@ class DatabaseManager {
     });
   }
 
+  static Future<void> deleteCurrentHousehold() async {
+    String householdCode = CurrentHousehold.getCurrentHouseholdId();
+    DatabaseReference householdRef = _databaseInstance
+        .ref("households/$householdCode");
+    await householdRef.remove();
+  }
   // ------------------------ END HOUSEHOLD OPERATIONS ------------------------
 
   // ------------------------ CHORE OPERATIONS ------------------------
